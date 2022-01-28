d66b1ff91ce8a78e8e46af78c8812839ab2e8f21
Danil <totem7h@gmail.com>
2022-01-28 15:33:21 +0400

    1.1. Job c параметрами [#260360 #256610]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -10,7 +10,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -20,10 +19,11 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class AlertRabbit {
 
     public Properties initProp() throws IOException {
-        InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties");
-        Properties prop = new Properties();
-        prop.load(in);
-        return prop;
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties prop = new Properties();
+            prop.load(in);
+            return prop;
+        }
     }
 
     public Connection initCon(Properties properties) throws Exception {
@@ -34,38 +34,29 @@ public class AlertRabbit {
         return DriverManager.getConnection(url, login, password);
     }
 
-    public int in(Properties properties) {
-        int second = 0;
-        try  {
-            second = Integer.parseInt(properties.getProperty("rabbit.interval"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return second;
-    }
-
     public static void main(String[] args) {
         try {
             AlertRabbit alertRabbit = new AlertRabbit();
-            var prop = alertRabbit.initProp();
-            var connect = alertRabbit.initCon(prop);
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDataMap data = new JobDataMap();
-            data.put("connection", connect);
-            JobDetail job = newJob(Rabbit.class)
-                    .usingJobData(data)
-                    .build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(alertRabbit.in(prop))
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
+            Properties prop = alertRabbit.initProp();
+            try (Connection connect = alertRabbit.initCon(prop)) {
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("connection", connect);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(Integer.parseInt(prop.getProperty("rabbit.interval")))
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -79,9 +70,6 @@ public class AlertRabbit {
 
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
-            System.out.println("Rabbit runs here ...");
-            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("connection");
-            store.add(System.currentTimeMillis());
             Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
             try (PreparedStatement statement = connection.prepareStatement("insert into rabbit(created_date) values(?)")) {
                 statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
