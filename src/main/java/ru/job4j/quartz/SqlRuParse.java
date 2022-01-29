eddf5ba4aebb571f1339eef1368926029ccaae60
Danil <totem7h@gmail.com>
2022-01-29 15:40:26 +0400

    2.4. SqlRuParse [#285213 #257390]

--- src/main/java/ru/job4j/quartz/SqlRuParse.java
+++ src/main/java/ru/job4j/quartz/SqlRuParse.java
@@ -17,23 +17,27 @@ public class SqlRuParse implements Parse {
     }
 
     public static void main(String[] args) throws Exception{
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            Element parent = td.parent();
-            String linkVacancy = href.attr("href");
-            System.out.println(linkVacancy);
-            /*System.out.println(href.text());
-            System.out.println(parent.children().get(5).text());*/
-            /*SqlRuParse ruParse = new SqlRuParse();
-            ruParse.list(linkVacancy);*/
+        for (int i = 1; i < 6; i++) {
+            String link = "https://www.sql.ru/forum/job-offers/" + i;
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            SqlRuParse ruParse = new SqlRuParse();
+            List<Post> list = ruParse.list(link);
+            /*for (Element td : row){
+                Element href = td.child(0);
+                Element parent = td.parent();
+                String linkVacancy = href.attr("href");
+                System.out.println(linkVacancy);
+            System.out.println(href.text());
+            System.out.println(parent.children().get(5).text());
+            }*/
         }
     }
 
     @Override
     public List<Post> list(String link) {
         List<Post> posts = new ArrayList<>();
+        /*Получить ссылку из href = ссылки на пост и её передать в detail*/
         posts.add(detail(link));
         return posts;
     }
@@ -42,8 +46,14 @@ public class SqlRuParse implements Parse {
     public Post detail(String link) {
         Post post = null;
         try {
-            PartsLoading parts = new PartsLoading();
-            post = parts.searchData(link);
+            SqlRuParse ruParse = new SqlRuParse();
+            Document doc = Jsoup.connect(link).get();
+            String description  = doc.select(".msgBody").get(1).text();
+            String date = doc.select(".msgFooter").get(0).text();
+            date = date.substring(0, date.indexOf('[')).trim();
+            String title = doc.select(".messageHeader").get(0).text();
+
+            post = Post(title, link, description, parser.parse(date));
         } catch (Exception e) {
             e.printStackTrace();
         }
