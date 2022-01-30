2a6a6e8d8835f7f800f5879257e335377c2f84fe
Danil <totem7h@gmail.com>
2022-01-30 17:15:41 +0400

    2.4. SqlRuParse [#285213 #257390]

--- src/main/java/ru/job4j/quartz/SqlRuParse.java
+++ src/main/java/ru/job4j/quartz/SqlRuParse.java
@@ -5,40 +5,45 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SqlRuParse implements Parse {
 
-    private final DateTimeParser dateTimeParser;
+    /*private final DateTimeParser dateTimeParser;
 
     public SqlRuParse(DateTimeParser dateTimeParser) {
         this.dateTimeParser = dateTimeParser;
-    }
+    }*/
 
-    public static void main(String[] args) throws Exception{
-        for (int i = 1; i < 6; i++) {
-            String link = "https://www.sql.ru/forum/job-offers/" + i;
-            Document doc = Jsoup.connect(link).get();
-            Elements row = doc.select(".postslisttopic");
-            SqlRuParse ruParse = new SqlRuParse();
-            List<Post> list = ruParse.list(link);
-            /*for (Element td : row){
-                Element href = td.child(0);
-                Element parent = td.parent();
-                String linkVacancy = href.attr("href");
-                System.out.println(linkVacancy);
-            System.out.println(href.text());
-            System.out.println(parent.children().get(5).text());
-            }*/
-        }
+    public static void main(String[] args) {
+        String link = "https://www.sql.ru/forum/job-offers/";
+        SqlRuParse ruParse = new SqlRuParse();
+        ruParse.list(link);
     }
 
     @Override
-    public List<Post> list(String link) {
+    public List<Post> list(String link)  {
         List<Post> posts = new ArrayList<>();
-        /*Получить ссылку из href = ссылки на пост и её передать в detail*/
-        posts.add(detail(link));
+        try {
+            for (int i = 1; i < 6; i++) {
+                Document doc = Jsoup.connect(link + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    String namePost = td.text();
+                    String linkPost = td.child(0).attr("href"); // ссылки на пост
+                    if (namePost.contains("Java") && !namePost.contains("JavaScript")) {
+                        /*System.out.println(linkPost);
+                        System.out.println(namePost);*/
+                        posts.add(detail(linkPost));
+                    }
+                }
+                /*System.out.println("******************");*/
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return posts;
     }
 
@@ -46,14 +51,15 @@ public class SqlRuParse implements Parse {
     public Post detail(String link) {
         Post post = null;
         try {
-            SqlRuParse ruParse = new SqlRuParse();
             Document doc = Jsoup.connect(link).get();
             String description  = doc.select(".msgBody").get(1).text();
             String date = doc.select(".msgFooter").get(0).text();
             date = date.substring(0, date.indexOf('[')).trim();
             String title = doc.select(".messageHeader").get(0).text();
 
-            post = Post(title, link, description, parser.parse(date));
+            SqlRuDateTimeParser dateTimeParser = new SqlRuDateTimeParser();
+            LocalDateTime created = dateTimeParser.parse(date);
+            post = new Post(title, link, description, created);
         } catch (Exception e) {
             e.printStackTrace();
         }
