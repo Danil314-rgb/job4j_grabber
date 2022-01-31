1155f9aa8826f8e20e6c58415af85691e12a8284
Danil <totem7h@gmail.com>
2022-01-31 13:30:40 +0400

    2. SQL Select by company [#2760]

---
+++ sql/t.sql
@@ -0,0 +1,46 @@
+CREATE TABLE company( 
+    id integer NOT NULL,
+    name character varying,
+    CONSTRAINT company_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE person(
+    id integer NOT NULL,
+    name character varying,
+    company_id integer references company(id),
+    CONSTRAINT person_pkey PRIMARY KEY (id)
+);
+
+insert into company(id, name) values(1, 'Apple');
+insert into company(id, name) values(2, 'Google');
+insert into company(id, name) values(3, 'Amazone');
+insert into company(id, name) values(4, 'Ozon');
+insert into company(id, name) values(5, 'Fonbet');
+
+insert into person(id, name, company_id) values(1, 'Tom', 3);
+insert into person(id, name, company_id) values(2, 'Bob', 3);
+insert into person(id, name, company_id) values(3, 'Ann', 2);
+insert into person(id, name, company_id) values(4, 'Tommi', 2);
+insert into person(id, name, company_id) values(5, 'Kent', 4);
+insert into person(id, name, company_id) values(6, 'Kot', 5);
+
+select p.name, c.name
+from person p
+left join company c
+on c.id = p.company_id
+where p.company_id != 5;
+
+with g AS (
+select company.name, count(person) as c 
+	from company 
+	inner join person 
+	on (company.id = person.company_id) 
+	group by company.name
+)
+select * from g where g.c = (select max(g.c) from g);
+
+
+
+
+
+
